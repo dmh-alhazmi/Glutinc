ea8bc3f687c3a3aa3c98b21b6d05b6a4e0ce170c
dmh-alhazmi <dmh.alhazmi@gmail.com>
2025-12-02 10:30:40 +0300

    Initial Commit

---
+++ GlutincTeam/GlutincTeamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlutincTeamApp.swift
+//  GlutincTeam
+//
+//  Created by Deemah Alhazmi on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GlutincTeamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
